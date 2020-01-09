cc8424fd9d664f1d14e01c492587ca0e5b56eed7
Caleb Reyes <sciencecamp123@gmail.com>
2020-01-08 16:53:53 -0800

    Added CarsAndCats with EJS and views

--- Express/CarsAndCats/server.js
+++ Express/CarsAndCats/server.js
@@ -6,16 +6,15 @@ app.get('/', (request, response) => {
 });
 
 
-// app.get("/users", (req, res) => {
-//     // hard-coded user data
-//     var users_array = [
-//         {name: "Michael", email: "michael@codingdojo.com"}, 
-//         {name: "Jay", email: "jay@codingdojo.com"}, 
-//         {name: "Brendan", email: "brendan@codingdojo.com"}, 
-//         {name: "Andrew", email: "andrew@codingdojo.com"}
-//     ];
-//     res.render('users', {users: users_array});
-// })
+app.get("/cars", (req, res) => {
+    res.render('cars');
+})
+app.get("/cats", (req, res) => {
+    res.render('cats');
+})
+app.get("/cars/new", (req, res) => {
+    res.render('form');
+})
 
 
 app.use(express.static(__dirname + "/static"));
