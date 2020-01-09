0bec40abe8b99a99f5ac77df7f66d4394f8b72ee
Caleb Reyes <sciencecamp123@gmail.com>
2020-01-08 18:19:37 -0800

    Added the EJSCatData assignment

--- Express/CarsAndCats/server.js
+++ Express/CarsAndCats/server.js
@@ -17,6 +17,28 @@ app.get("/cars/new", (req, res) => {
 })
 
 
+
+app.get("/cat1", (req, res) => {
+   var cat1 = [
+      {name: "cat1", food: "spaghetti", age: "3", sleep: ["under the bed", "in a sunbeam", "under the table"], image_source: "cats"}
+   ]
+   res.render('details', {cat: cat1});
+})
+app.get("/cat2", (req, res) => {
+   var cat2 = [
+      {name: "cat2", food: "tacos", age: "5", sleep: ["by the sofa", "in a towel", "on the roof"], image_source: "cat2"}
+   ]
+   res.render('details', {cat: cat2});
+})
+app.get("/cat3", (req, res) => {
+   var cat3 = [
+      {name: "cat3", food: "cake", age: "20", sleep: ["in the kitchen", "in a tree", "on the desk"], image_source: "cat3"}
+   ]
+   res.render('details', {cat: cat3});
+})
+
+
+
 app.use(express.static(__dirname + "/static"));
 
 app.set('view engine', 'ejs');
