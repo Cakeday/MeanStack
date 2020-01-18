45a49ced72ae40fc2c6be33d03e9cf9e0d69fe48
Caleb Reyes <sciencecamp123@gmail.com>
2020-01-17 19:02:04 -0800

    Added Angular Restful Tasks

--- MongoDB/Mongoose/TaskAPI/public/src/app/app.component.ts
+++ MongoDB/Mongoose/TaskAPI/public/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpService } from './http.service';
 
 @Component({
@@ -6,7 +6,60 @@ import { HttpService } from './http.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+
   title = 'public';
-  constructor(private _httpService: HttpService){}
+  tasks = [];
+
+  constructor(private _httpService: HttpService){
+
+  }
+
+  ngOnInit(){
+    this.getTasksFromService();
+  }
+
+  getTasksFromService(){
+    let observable = this._httpService.getTasks();
+    observable.subscribe(data => {
+      console.log("Got our data!", data);
+      for(let key in data){
+        this.tasks.push(data[key])
+      }
+    })
+  }
+
+  getOneTaskFromService(id){
+    let observable = this._httpService.getOneTask(id);
+    observable.subscribe(data => {
+      console.log("Got our single data!", data);
+      this.tasks = data['one_task']
+    })
+  }
+
+  createTaskFromService(){
+    let observable = this._httpService.createTask();
+    observable.subscribe(data => {
+      console.log("created our data!", data);
+      this.tasks = data['created']
+    })
+  }
+
+  updateTaskFromService(id){
+    let observable = this._httpService.updateTask(id);
+    observable.subscribe(data => {
+      console.log("updated our data!", data);
+      this.tasks = data['updated']
+    })
+  }
+
+  deleteTasksFromService(id){
+    let observable = this._httpService.deleteTask(id);
+    observable.subscribe(data => {
+      console.log("deleted our data!", data);
+      this.tasks = data['tasks']
+    })
+  }
+
 }
